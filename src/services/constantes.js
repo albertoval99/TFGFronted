81748d84bfbe366a91fcff556c4b67856326cb23
albertoval99 <albertovalabad@gmail.com>
2025-04-29 17:25:16 +0200

    costantes

--- src/services/constantes.js
+++ src/services/constantes.js
@@ -1,11 +1,10 @@
-const URL  = "http://localhost:3000/api";
+//const URL  = "http://localhost:3000/api";
 
 //APACHE
-//const URL = "http://192.168.20.134:3001/api";
+const URL = "http://192.168.20.134:3001/api";
 /**
  * IP para ver front: http://192.168.206.157:8080/
  * Hacer el npm run build en esta terminal wsl
- * PARA npm run dev del back  en el cmd de windows:C:\Users\alber\Desktop\TFGBackend>npm run dev
- HE CREADO EL FICHERO deploy.sh para que con  ./deploy.sh se vaya actualizando
+ * PARA npm run dev del back  en el cmd de windows,btn derecho en icono y simbolo del sistema:C:\Users\alber\Desktop\TFGBackend>npm run dev
 */
 export { URL };
