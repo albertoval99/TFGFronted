4862470572d96bc6e65acc675e7490f0cf1d7209
albertoval99 <albertovalabad@gmail.com>
2025-05-26 12:45:36 +0200

    constante cambiada

--- src/services/constantes.js
+++ src/services/constantes.js
@@ -3,8 +3,8 @@
 //APACHE
 //const URL = "http://192.168.20.135:3002/api";
 
-//DESPLEGAR: https://dk0t306hy0.execute-api.us-east-1.amazonaws.com/alberto
-const URL = "https://dk0t306hy0.execute-api.us-east-1.amazonaws.com/alberto/api";
+//DESPLEGAR: https://qdg6e8tm36.execute-api.us-east-1.amazonaws.com/alberto/api
+const URL = "https://qdg6e8tm36.execute-api.us-east-1.amazonaws.com/alberto/api";
 
 /**
  * IP para ver front: http://192.168.206.157:8080/
