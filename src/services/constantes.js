35554c810e0ae32d3eb30d7b8555403e70709f49
albertoval99 <albertovalabad@gmail.com>
2025-05-26 12:24:06 +0200

    Desplegando

--- src/services/constantes.js
+++ src/services/constantes.js
@@ -1,7 +1,11 @@
 //const URL  = "http://localhost:3000/api";
 
 //APACHE
-const URL = "http://192.168.20.135:3002/api";
+//const URL = "http://192.168.20.135:3002/api";
+
+//DESPLEGAR: https://dk0t306hy0.execute-api.us-east-1.amazonaws.com/alberto
+const URL = "https://dk0t306hy0.execute-api.us-east-1.amazonaws.com/alberto/api";
+
 /**
  * IP para ver front: http://192.168.206.157:8080/
  * Hacer el npm run build en esta terminal wsl
