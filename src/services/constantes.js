971a7e5513ca766b90e0d3b5a250eebffc0dbb71
albertoval99 <albertovalabad@gmail.com>
2025-05-31 14:41:38 +0200

    calendario y constante

--- src/services/constantes.js
+++ src/services/constantes.js
@@ -1,10 +1,10 @@
-//const URL  = "http://localhost:3000/api";
+const URL  = "http://localhost:3000/api";
 
 //APACHE
 //const URL = "http://192.168.20.135:3002/api";
 
 //DESPLIEGUE.
-const URL = "https://qdg6e8tm36.execute-api.us-east-1.amazonaws.com/alberto/api";
+//const URL = "https://qdg6e8tm36.execute-api.us-east-1.amazonaws.com/alberto/api";
 
 /**
  * IP para ver front: http://192.168.206.157:8080/
